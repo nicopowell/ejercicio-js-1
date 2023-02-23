418cb1c85491d4c204695579f0b12037b2de5cdc
Nicolas Powell <nicopowell30@gmail.com>
2023-02-22 22:09:08 -0300

    terminé el ejercicio 6

---
+++ js/ejercicio-6.js
@@ -0,0 +1,12 @@
+// Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+
+let num1 = prompt("Ingrese el primer número: ");
+let num2 = prompt("Ingrese el segundo número: ");
+
+if(num1 > num2){
+    document.write(num1 + " es mayor que " + num2)
+} else if (num2 > num1){
+    document.write(num2 + " es mayor que " + num1)
+} else {
+    document.write("Los números ingresados son iguales")
+}
