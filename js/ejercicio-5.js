673065f90343c7384e019bada41cbee70a6052c0
Nicolas Powell <nicopowell30@gmail.com>
2023-02-22 22:08:59 -0300

    terminé el ejercicio 5

---
+++ js/ejercicio-5.js
@@ -0,0 +1,5 @@
+// Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+let num1 = parseInt(prompt("Ingrese el primer número: "));
+let num2 = parseInt(prompt("Ingrese el segundo número: "));
+document.write(num1 + num2);
